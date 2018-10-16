52647626abd02b923715ac054c42b631893a9e1a
Allan Leal <allanmulin@gmail.com>
2018-10-16 08:23:50 +0200

    Added tests for serialization of ChemicalFormula and Element

--- Atomik/Serialization.test.cxx
+++ Atomik/Serialization.test.cxx
@@ -22,8 +22,34 @@
 #include <sstream>
 
 // Atomik includes
+#include <Atomik/ChemicalFormula.hpp>
+#include <Atomik/Serialization.hpp>
+#include <Atomik/Element.hpp>
+using namespace Atomik;
 
-TEST_CASE("Testing serialization of substances", "[Serialization]")
+TEST_CASE("Testing serialization of ChemicalFormula", "[Serialization]")
 {
+    ChemicalFormula formula;
 
+    std::istringstream iss("CaCO3");
+    iss >> formula;
+
+    CHECK(formula.str() == "CaCO3");
+    CHECK(formula.coefficient("Ca") == 1);
+    CHECK(formula.coefficient("C") == 1);
+    CHECK(formula.coefficient("O") == 3);
+}
+
+TEST_CASE("Testing serialization of Element", "[Serialization]")
+{
+    Element element;
+
+    std::istringstream iss("H Hydrogen 1 1.00794 2.2");
+    iss >> element;
+
+    CHECK(element.symbol() == "H");
+    CHECK(element.name() == "Hydrogen");
+    CHECK(element.atomicNumber() == 1);
+    CHECK(element.atomicWeight() == 1.00794);
+    CHECK(element.electronegativity() == 2.2);
 }
